3fa8fbf877d8f6f65694daebbbcca48808bf49d1
Juliana Aquino <juliana.aquino@f1rst.com.br>
2022-11-19 23:32:43 -0300

    Removing unused methods

--- src/br/com/ada/bookstore/database/factory/Database.java
+++ src/br/com/ada/bookstore/database/factory/Database.java
@@ -35,8 +35,7 @@ public class Database {
         }
     }
 
-    /*@Override
-    public Product findById(Long id) {
+    /*public Product findById(Long id) {
         Product foundedItem = null;
         for (Product item: list) {
             if (item.getId() == id) {
@@ -45,18 +44,6 @@ public class Database {
         }
         return foundedItem;
     }*/
-
-    public List<Product> findAll() {
-        return Collections.unmodifiableList(list);
-    }
-
-    public void remove(Long id) {
-        Product foundedItem = this.findById(id);
-        if (foundedItem != null) {
-            list.remove(foundedItem);
-        }
-    }
-
     public <T, K> T findById(K id) {
         Product foundedItem = null;
         for (Product item: list) {
@@ -67,76 +54,22 @@ public class Database {
         return (T) foundedItem;
     }
 
-    public <K> void remove(K id) {
-        Product foundedItem = this.findById(id);
-        if (foundedItem != null) {
-            list.remove(foundedItem);
-        }
-    }
-
-
-    //////
-
-
-    /*public void save(Product item) {
-        Product product = (Product) item;
-        product.setId(++id);
-        list.add(product);
-    }*/
-
-    /*public void update(Product product) {
-        Product foundedItem = this.findById(product.getId());
-        if (foundedItem != null) {
-            foundedItem = product;
-        }
-    }*/
-
-    /*@Override
-    public Product findById(K id) {
-        return null;
-    }
-
-    public Product findById(Long id) {
-        Product foundedItem = null;
-        for (Product item: list) {
-            if (item.getId() == id) {
-                foundedItem = item;
-            }
-        }
-        return foundedItem;
-    }
-
     public List<Product> findAll() {
         return Collections.unmodifiableList(list);
     }
 
-    @Override
-    public void remove(K id) {
-
-    }
-
-    public void remove(Long id) {
-        Product foundedItem = this.findById(id);
-        if (foundedItem != null) {
-            list.remove(foundedItem);
-        }
-    }
-
-    public <T, K> T findById(K id) {
-        Product foundedItem = null;
-        for (Product item: list) {
-            if (item.getId() == id) {
-                foundedItem = item;
-            }
-        }
-        return (T) foundedItem;
-    }
-
-    public <K> void remove(K id) {
+    /*public void remove(Long id) {
         Product foundedItem = this.findById(id);
         if (foundedItem != null) {
             list.remove(foundedItem);
         }
     }*/
 
+    public <K> void remove(K id) {
+        Product foundedItem = this.findById(id);
+        if (foundedItem != null) {
+            list.remove(foundedItem);
+        }
+    }
+    
 }
