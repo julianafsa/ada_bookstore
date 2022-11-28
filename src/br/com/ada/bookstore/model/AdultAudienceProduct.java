70b1b172732148f93423767a37ff01d1b71f21e0
Juliana Aquino <juliana.aquino@f1rst.com.br>
2022-11-28 14:33:49 -0300

    Adding a solution for second stage. Adding a interface for adult audience product. Changing order service impl in order to calculate discount and verify age of customer.

---
+++ src/br/com/ada/bookstore/model/AdultAudienceProduct.java
@@ -0,0 +1,7 @@
+package br.com.ada.bookstore.model;
+
+public interface AdultAudienceProduct {
+
+    Boolean hasAdultAudience();
+    void setHasAdultAudience(Boolean hasAdultAudience);
+}
---
+++ src/br/com/ada/bookstore/model/customer/Customer.java
@@ -0,0 +1,59 @@
+package br.com.ada.bookstore.model.customer;
+
+import java.time.LocalDate;
+
+public class Customer {
+    private String name;
+    private String rg;
+    private String cpf;
+    private LocalDate birthdate;
+
+    public Customer(String name, String rg, String cpf, LocalDate birthdate) {
+        this.name = name;
+        this.rg = rg;
+        this.cpf = cpf;
+        this.birthdate = birthdate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", rg='" + rg + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", birthdate=" + birthdate +
+                '}';
+    }
+}
